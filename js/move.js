47bbd8cd9259bd2adc1af0cb50989398c276c136
iva891 <57774593+iva891@users.noreply.github.com>
2020-07-27 12:59:18 +0300

    Исправления

--- js/move.js
+++ js/move.js
@@ -3,51 +3,56 @@
 
   var setupDialogElement = document.querySelector('.setup');
   var dialogHandle = setupDialogElement.querySelector('.upload');
+  var startCoords = {
+    x: 0,
+    y: 0
+  };
+  var dragged;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+    setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+        clickEvt.preventDefault();
+        dialogHandle.removeEventListener('click', onClickPreventDefault);
+      };
+      dialogHandle.addEventListener('click', onClickPreventDefault);
+    }
+  };
 
   var onDialogHandleClick = function (evt) {
     evt.preventDefault();
 
-    var startCoords = {
+    startCoords = {
       x: evt.clientX,
       y: evt.clientY
     };
 
-    var dragged = false;
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      dragged = true;
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
-      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
-
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-
-      if (dragged) {
-        var onClickPreventDefault = function (clickEvt) {
-          clickEvt.preventDefault();
-          dialogHandle.removeEventListener('click', onClickPreventDefault);
-        };
-        dialogHandle.addEventListener('click', onClickPreventDefault);
-      }
-    };
+    dragged = false;
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
